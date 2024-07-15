68b4e787cbf3d8d586ec91b692c34349b3143fe2
Weigand, Shawn <sweigand@estee.com>
2024-07-15 16:24:03 -0400

    begin add typescript and tailwind

---
+++ server/server.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const todosRoute = require('./routes/todos');
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb://localhost:27017/mern-stack-db";
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+// Define routes and middleware
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/todos', todosRoute);
